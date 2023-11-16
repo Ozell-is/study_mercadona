bd63212c835a16c911b24205904171796aa8c1a5
kevin <kevinlaratte02@gmail.com>
2023-11-16 12:50:40 +0100

    dernier push avant deploiement

--- app.py
+++ app.py
@@ -13,34 +13,33 @@ from controller.Logoutws import logout_ws
 from controller.Productws import product_ws
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
 
-    app.secret_key = "t55(4y5y45y45-tg4z"
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    app.register_blueprint(admin_ws)
-    app.register_blueprint(product_ws)
-    app.register_blueprint(category_ws)
-    app.register_blueprint(login_ws)
-    app.register_blueprint(logout_ws)
+app.secret_key = "t55(4y5y45y45-tg4z"
 
-    # configuration token JWT
-    app.config["JWT_SECRET_KEY"] = "hre5h4rz54teaz564"
-    # Empeche les modification exterieur
-    app.config["JWT_COOKIE_CSRF_PROTECT"] = True
-    app.config["JWT_ACCESS_COOKIE_PATH"] = "/api"
-    app.config["JWT_REFRESH_COOKIE_PATH"] = "/api/tokenRefresh"
-    app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
-    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
+app.register_blueprint(admin_ws)
+app.register_blueprint(product_ws)
+app.register_blueprint(category_ws)
+app.register_blueprint(login_ws)
+app.register_blueprint(logout_ws)
 
-    db.init_app(app)
-    jwt = JWTManager(app)
+# configuration token JWT
+app.config["JWT_SECRET_KEY"] = "hre5h4rz54teaz564"
+# Empeche les modification exterieur
+app.config["JWT_COOKIE_CSRF_PROTECT"] = True
+app.config["JWT_ACCESS_COOKIE_PATH"] = "/api"
+app.config["JWT_REFRESH_COOKIE_PATH"] = "/api/tokenRefresh"
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
 
-    with app.app_context():
-        db.create_all()
+db.init_app(app)
+jwt = JWTManager(app)
 
-    return app
+with app.app_context():
+    db.create_all()
+
 
 
 if __name__ == "__main__":
